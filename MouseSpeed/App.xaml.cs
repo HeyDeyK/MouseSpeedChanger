3b5867fbaacb6c71e0e9ad939a83c71401dc52a0
vileton15 <vileton15@sps-prosek.cz>
2018-06-14 12:30:37 +0200

    cs

--- MouseSpeed/App.xaml.cs
+++ MouseSpeed/App.xaml.cs
@@ -1,17 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace MouseSpeed
 {
     /// <summary>
     /// Interakční logika pro App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        private NotifyIcon _notifyIcon;
+        private bool _isExit;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+            _notifyIcon.Icon = MouseSpeed.Properties.Resources.mouse;
+            _notifyIcon.Visible = true;
+
+            CreateContextMenu();
+        }
+
+        private void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip =
+              new System.Windows.Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("Nastavení myši").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Opustit").Click += (s, e) => ExitApplication();
+        }
+
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
     }
 }
--- MouseSpeed/AppViewModel.cs
+++ MouseSpeed/AppViewModel.cs
@@ -12,15 +12,23 @@ namespace MouseSpeed
 {
     class AppViewModel : INotifyPropertyChanged
     {
+        public bool SwapButtons = false;
         public const UInt32 SPI_SETMOUSESPEED = 0x0071;
+        public const UInt32 SPI_SETWHEELSCROLLLINES = 0x0069;
+        public const UInt32 SPI_SETMOUSEBUTTONSWAP = 0x0021;
+        public const UInt32 SPI_SETDOUBLECLICKTIME = 0x0020;
+
         [DllImport("user32.dll")]
         static extern Boolean SystemParametersInfo(
             UInt32 uiAction,
             UInt32 uiParam,
             UInt32 pvParam,
             UInt32 fWinIni);   // Not used     
+        
 
-        private double _SliderPomerValue = 1;
+
+
+        private double _SliderPomerValue = 5;
         public double SliderPomerValue
         {
             get => _SliderPomerValue;
@@ -35,6 +43,36 @@ namespace MouseSpeed
                 OnPropertyChanged("SliderPomerValue");
             }
         }
+        private double _SliderWheelSpeed = 3;
+        public double SliderWheelSpeed
+        {
+            get => _SliderWheelSpeed;
+            set
+            {
+                double StepValue = 1;
+                var newStep = Math.Round(value / StepValue);
+
+                _SliderWheelSpeed = newStep * StepValue;
+                Console.WriteLine((int)SliderWheelSpeed);
+                ChangeWheelSpeed((int)SliderWheelSpeed);
+                OnPropertyChanged("SliderWheelSpeed");
+            }
+        }
+        private double _SliderDoubleClickSpeed = 5;
+        public double SliderDoubleClickSpeed
+        {
+            get => _SliderDoubleClickSpeed;
+            set
+            {
+                double StepValue = 1;
+                var newStep = Math.Round(value / StepValue);
+
+                _SliderDoubleClickSpeed = newStep * StepValue;
+                Console.WriteLine((int)SliderDoubleClickSpeed);
+                ChangeDoubleSpeed((int)SliderDoubleClickSpeed * 1000);
+                OnPropertyChanged("SliderDoubleClickSpeed");
+            }
+        }
         private void ChangeMouseSpeed(int cislo)
         {
             const UInt32 SPI_SETMOUSESPEED = 0x0071;
@@ -46,10 +84,47 @@ namespace MouseSpeed
                 MOUSESPEED,
                 0);
         }
-        public RelayCommand ClickCommand { get; }
+        private void ChangeWheelSpeed(int cislo)
+        {
+            const UInt32 SPI_SETWHEELSCROLLLINES = 0x0069;
+            UInt32 WHEELSPEED = Convert.ToUInt32(cislo);
+
+            SystemParametersInfo(
+                SPI_SETWHEELSCROLLLINES,
+                WHEELSPEED,
+                0,
+                0);
+        }
+        private void ChangeSwapButtons(bool zmena)
+        {
+            const UInt32 SPI_SETMOUSEBUTTONSWAP = 0x0021;
+            UInt32 SWAPBUTTONS = Convert.ToUInt32(zmena);
+
+
+            SystemParametersInfo(
+                SPI_SETMOUSEBUTTONSWAP,
+                SWAPBUTTONS,
+                0,
+                0);
+        }
+        private void ChangeDoubleSpeed(int cislo)
+        {
+            const UInt32 SPI_SETDOUBLECLICKTIME = 0x0020;
+            UInt32 DOUBLECLICKSPEED = Convert.ToUInt32(cislo);
+
+
+            SystemParametersInfo(
+                SPI_SETDOUBLECLICKTIME,
+                DOUBLECLICKSPEED,
+                0,
+                0);
+        }
+        public RelayCommand ClickCommand { get; set; }
+        public RelayCommand CmdSwapButtons { get; set; }
         public AppViewModel()
         {
             ClickCommand = new RelayCommand(ClickMethod);
+            CmdSwapButtons = new RelayCommand(SwapMethod);
 
         }
         private void ClickMethod()
@@ -64,6 +139,22 @@ namespace MouseSpeed
                 0);
             SliderPomerValue = 5;
             OnPropertyChanged("SliderPomerValue");
+        }
+        public void SwapMethod()
+        {
+            Console.WriteLine("menim");
+            if (SwapButtons)
+            {
+                SwapButtons = false;
+                ChangeSwapButtons(SwapButtons);
+                OnPropertyChanged("SwapButtons");
+            }
+            else
+            {
+                SwapButtons = true;
+                ChangeSwapButtons(SwapButtons);
+                OnPropertyChanged("SwapButtons");
+            }
         }
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string name)
